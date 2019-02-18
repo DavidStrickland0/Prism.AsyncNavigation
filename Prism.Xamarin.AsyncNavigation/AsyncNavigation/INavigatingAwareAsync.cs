6df1e6a05f271f500aaacd81673e6cf3a627aed0
David.Strickland <Davidstrickland0@hotmail.com>
2019-02-18 19:04:41 +0000

    Initial Attempt at a AsyncAware onNavigating

---
+++ Prism.Xamarin.AsyncNavigation/AsyncNavigation/INavigatingAwareAsync.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Navigation;
+
+namespace Prism.Xamarin.AsyncNavigation
+{
+    interface INavigatingAwareAsync
+    {
+        /// <summary>
+        /// Called before the implementor has been navigated to.
+        /// </summary>
+        /// <param name="parameters">The navigation parameters.</param>
+        /// <remarks>Not called when using device hardware or software back buttons</remarks>
+        Task OnNavigatingToAsync(INavigationParameters parameters);
+    }
+}
